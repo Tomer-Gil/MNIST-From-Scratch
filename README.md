59f5b823335b2da8182a53c1a5fe8f56dd27ab8f
tomer <tomer.gil@mail.huji.ac.il>
2024-04-19 19:46:34 +0300

    Initial commit
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   main.py

---
+++ README.md
@@ -0,0 +1,6 @@
+# NN built using only numpy and math, trained on the MNIST dataset
+
+credit to this guide:  
+https://www.youtube.com/watch?v=w8yWXqWQYmU
+
+His code had a few bugs I fixed on my own
---
+++ main.py
@@ -0,0 +1,138 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+
+def get_MNIST():
+    # Load MNIST from Kaggle from url: https://www.kaggle.com/c/digit-recognizer/data
+    # TODO: load it from url
+    url = r"train.csv/train.csv"
+    data = pd.read_csv(url)
+    return data
+
+
+# Get MNIST dataset ad pandas df
+data = get_MNIST()
+
+# Convert from Pandas to Numpy
+data = np.array(data)
+
+# Split to train and dev (cross-validation) sets
+m, n = data.shape
+np.random.shuffle(data)
+
+data_dev = data[0:1000].T
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+
+data_train = data[1000:m].T
+Y_train = data_train[0]
+X_train = data_train[1:n]
+
+
+def init_params():
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
+    return W1, b1, W2, b2
+
+
+def ReLU(Z):
+    return np.maximum(0, Z)
+
+
+def softmax(Z):
+    # Actually, if we calculate the sum, we can implement it this way:
+    # Subtract the sum from each element. Then, apply the exp function.
+    # Now exp(z_i - sum) = exp(z_i) / ex(sum), as wanted.
+    # return np.exp(Z - np.sum(Z))
+    return np.exp(Z) / np.sum(np.exp(Z), axis=0)
+
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+
+def deriv_ReLU(Z):
+    return Z > 0
+
+
+def back_prop(Z1, A1, Z2, A2, W2, X, Y):
+    m = Y.size
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+
+def update_parms(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+
+def get_prediction(A2):
+    return np.argmax(A2, 0)
+
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+
+def gradient_descent(X, Y, iterations, alpha):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W2, X, Y)
+        W1, b1, W2, b2 = update_parms(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 50 == 0:
+            print("Iteration: ", i)
+            print("Accuracy: ", get_accuracy(get_prediction(A2), Y))
+    return W1, b1, W2, b2
+
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.1)
+
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_prediction(A2)
+    return predictions
+
+
+def test_predictions(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
+# test_predictions(5, W1, b1, W2, b2)
+# test_predictions(2, W1, b1, W2, b2)
+# test_predictions(8, W1, b1, W2, b2)
+
+# dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
+# print(get_accuracy(dev_predictions, Y_dev))
